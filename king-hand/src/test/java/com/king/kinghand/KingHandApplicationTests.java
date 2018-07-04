fc51ded7a75c1ce81b1343ff8f9eb0054b2b664f
王鑫 <wangxin@wangxindeMacBook-Pro.local>
2018-07-04 11:28:33 +0800

    first commit

---
+++ king-hand/src/test/java/com/king/kinghand/KingHandApplicationTests.java
@@ -0,0 +1,16 @@
+package com.king.kinghand;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KingHandApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
